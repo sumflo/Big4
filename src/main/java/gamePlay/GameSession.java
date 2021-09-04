4f30fe2d640000792b3ddca4cfb2171eb42da4cd
sumflo <real61madrid@gmail.com>
2021-09-04 16:34:12 +0200

    GameSession, PlayedCharacter update

--- src/main/java/gamePlay/GameSession.java
+++ src/main/java/gamePlay/GameSession.java
@@ -1,4 +1,26 @@
 package gamePlay;
 
 public class GameSession {
+
+    private int id;
+    private String gameEndDate;
+
+    public GameSession() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGameEndDate() {
+        return gameEndDate;
+    }
+
+    public void setGameEndDate(String gameEndDate) {
+        this.gameEndDate = gameEndDate;
+    }
 }
